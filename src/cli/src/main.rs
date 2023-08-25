36abe51fa6497fbb3601d7c737c255112897e4e0
Kyle Schreiber <kyle@80x24.net>
2023-08-25 00:59:47 -0700

    Fix argument parsing

--- src/cli/src/main.rs
+++ src/cli/src/main.rs
@@ -66,33 +66,47 @@ fn main() -> Result<(), anyhow::Error> {
             print_version();
             return Ok(());
         }
-        "enc" | "encrypt" => match parse_encrypt(args.as_slice()) {
-            Ok(opts) => commands::encrypt(opts)?,
-            Err(e) => print_error(&e)?,
-        },
-        "dec" | "decrypt" => match parse_decrypt(args.as_slice()) {
-            Ok(opts) => commands::decrypt(opts)?,
-            Err(e) => print_error(&e)?,
-        },
-        "key" => match parse_key(args.as_slice()) {
-            Ok(key_command) => match key_command {
-                KeyCommand::Generate(outfile, env_pass) => commands::gen_key(outfile, env_pass)?,
-                KeyCommand::ChangePass(priv_key, env_pass) => {
-                    commands::change_pass(priv_key, env_pass)?
-                }
-                KeyCommand::ExtractPub(priv_key, env_pass) => {
-                    commands::extract_pub(priv_key, env_pass)?
-                }
-            },
-            Err(e) => print_error(&e)?,
-        },
-        "pass" | "password" => match parse_password(args.as_slice()) {
-            Ok(pass_command) => match pass_command {
-                PasswordCommand::Encrypt(pass_opts) => commands::pass_encrypt(pass_opts)?,
-                PasswordCommand::Decrypt(pass_opts) => commands::pass_decrypt(pass_opts)?,
-            },
-            Err(e) => print_error(&e)?,
-        },
+        "enc" | "encrypt" => {
+            let args = slice_args(&args, 2);
+            match parse_encrypt(args) {
+                Ok(opts) => commands::encrypt(opts)?,
+                Err(e) => print_error(&e)?,
+            }
+        }
+        "dec" | "decrypt" => {
+            let args = slice_args(&args, 2);
+            match parse_decrypt(args) {
+                Ok(opts) => commands::decrypt(opts)?,
+                Err(e) => print_error(&e)?,
+            }
+        }
+        "key" => {
+            let args = slice_args(&args, 2);
+            match parse_key(args) {
+                Ok(key_command) => match key_command {
+                    KeyCommand::Generate(outfile, env_pass) => {
+                        commands::gen_key(outfile, env_pass)?
+                    }
+                    KeyCommand::ChangePass(priv_key, env_pass) => {
+                        commands::change_pass(priv_key, env_pass)?
+                    }
+                    KeyCommand::ExtractPub(priv_key, env_pass) => {
+                        commands::extract_pub(priv_key, env_pass)?
+                    }
+                },
+                Err(e) => print_error(&e)?,
+            }
+        }
+        "pass" | "password" => {
+            let args = slice_args(&args, 2);
+            match parse_password(args) {
+                Ok(pass_command) => match pass_command {
+                    PasswordCommand::Encrypt(pass_opts) => commands::pass_encrypt(pass_opts)?,
+                    PasswordCommand::Decrypt(pass_opts) => commands::pass_decrypt(pass_opts)?,
+                },
+                Err(e) => print_error(&e)?,
+            }
+        }
         _ => {
             return Err(anyhow!("{}", USAGE));
         }
@@ -115,6 +129,13 @@ fn convert_args<T: AsRef<std::ffi::OsStr>>(args: &[T]) -> Result<Vec<String>, an
     Ok(converted)
 }
 
+/// Return the remainder of the slice after the given index or the empty slice otherwise
+fn slice_args<'a, 'b: 'a>(args: &'a [&'b str], idx: usize) -> &'a [&'b str] {
+    let args: &'a [&'b str] = if args.len() > idx { &args[idx..] } else { &[] };
+
+    args
+}
+
 fn print_help() {
     println!("{}", USAGE);
 }
@@ -128,16 +149,6 @@ fn print_error(msg: &str) -> Result<(), anyhow::Error> {
 }
 
 fn parse_encrypt(args: &[&str]) -> Result<EncryptOptions, String> {
-    if args.len() < 2 {
-        return Err("Not enough arguments".to_string());
-    }
-
-    let extra = if args.len() == 3 {
-        &args[..3]
-    } else {
-        &args[3..]
-    };
-
     let mut encrypt_opts = Options::new();
     encrypt_opts.long_only(true);
     encrypt_opts.reqopt("t", "to", "Recipient key name", "NAME");
@@ -146,7 +157,7 @@ fn parse_encrypt(args: &[&str]) -> Result<EncryptOptions, String> {
     encrypt_opts.optopt("k", "keyring", "Keyring file", "FILE");
     encrypt_opts.optflag("", "env-pass", "read KESTREL_PASSWORD env var");
 
-    let matches = match encrypt_opts.parse(extra) {
+    let matches = match encrypt_opts.parse(args) {
         Ok(m) => m,
         Err(e) => return Err(e.to_string()),
     };
@@ -178,16 +189,6 @@ fn parse_encrypt(args: &[&str]) -> Result<EncryptOptions, String> {
 }
 
 fn parse_decrypt(args: &[&str]) -> Result<DecryptOptions, String> {
-    if args.len() < 2 {
-        return Err("Not enough arguments".to_string());
-    }
-
-    let extra = if args.len() == 3 {
-        &args[..3]
-    } else {
-        &args[3..]
-    };
-
     let mut decrypt_opts = Options::new();
     decrypt_opts.long_only(true);
     decrypt_opts.reqopt("t", "to", "Recipient key name", "NAME");
@@ -195,7 +196,7 @@ fn parse_decrypt(args: &[&str]) -> Result<DecryptOptions, String> {
     decrypt_opts.optopt("k", "keyring", "Keyring file", "FILE");
     decrypt_opts.optflag("", "env-pass", "read KESTREL_PASSWORD env var");
 
-    let matches = match decrypt_opts.parse(extra) {
+    let matches = match decrypt_opts.parse(args) {
         Ok(m) => m,
         Err(e) => return Err(e.to_string()),
     };
@@ -225,24 +226,20 @@ fn parse_decrypt(args: &[&str]) -> Result<DecryptOptions, String> {
 }
 
 fn parse_key(args: &[&str]) -> Result<KeyCommand, String> {
-    if args.len() < 3 {
-        return Err("Not enough arguments".to_string());
+    if args.len() < 1 {
+        return Err("Invalid usage".to_string());
     }
 
-    let extra = if args.len() == 3 {
-        &args[..3]
-    } else {
-        &args[3..]
-    };
-
-    match args[2] {
+    match args[0] {
         "gen" | "generate" => {
             let mut gen_opts = Options::new();
             gen_opts.long_only(true);
             gen_opts.reqopt("o", "output", "Output file", "FILE");
             gen_opts.optflag("", "env-pass", "read KESTREL_PASSWORD env var");
 
-            let matches = match gen_opts.parse(extra) {
+            let args = slice_args(args, 1);
+
+            let matches = match gen_opts.parse(args) {
                 Ok(m) => m,
                 Err(e) => return Err(e.to_string()),
             };
@@ -257,7 +254,9 @@ fn parse_key(args: &[&str]) -> Result<KeyCommand, String> {
             change_opts.long_only(true);
             change_opts.optflag("", "env-pass", "read KESTREL_PASSWORD env var");
 
-            let matches = match change_opts.parse(extra) {
+            let args = slice_args(args, 1);
+
+            let matches = match change_opts.parse(args) {
                 Ok(m) => m,
                 Err(e) => return Err(e.to_string()),
             };
@@ -277,7 +276,9 @@ fn parse_key(args: &[&str]) -> Result<KeyCommand, String> {
             extract_opts.long_only(true);
             extract_opts.optflag("", "env-pass", "read KESTREL_PASSWORD env var");
 
-            let matches = match extract_opts.parse(extra) {
+            let args = slice_args(args, 1);
+
+            let matches = match extract_opts.parse(args) {
                 Ok(m) => m,
                 Err(e) => return Err(e.to_string()),
             };
@@ -292,30 +293,30 @@ fn parse_key(args: &[&str]) -> Result<KeyCommand, String> {
 
             Ok(KeyCommand::ExtractPub(private_key, env_pass))
         }
-        _ => Err("Invaild argument".to_string()),
+        _ => Err("Invalid argument".to_string()),
     }
 }
 
 fn parse_password(args: &[&str]) -> Result<PasswordCommand, String> {
-    if args.len() < 3 {
-        return Err("Not enough arguments".to_string());
+    if args.len() < 1 {
+        return Err("Invalid usage".to_string());
     }
 
-    let extra = if args.len() == 3 {
-        &args[..3]
-    } else {
-        &args[3..]
-    };
-
-    match args[2] {
-        "encrypt" | "enc" => match parse_pass_encrypt(extra) {
-            Ok(pass_opts) => Ok(PasswordCommand::Encrypt(pass_opts)),
-            Err(e) => Err(e),
-        },
-        "decrypt" | "dec" => match parse_pass_decrypt(extra) {
-            Ok(pass_opts) => Ok(PasswordCommand::Decrypt(pass_opts)),
-            Err(e) => Err(e),
-        },
+    match args[0] {
+        "encrypt" | "enc" => {
+            let args = slice_args(args, 1);
+            match parse_pass_encrypt(args) {
+                Ok(pass_opts) => Ok(PasswordCommand::Encrypt(pass_opts)),
+                Err(e) => Err(e),
+            }
+        }
+        "decrypt" | "dec" => {
+            let args = slice_args(args, 1);
+            match parse_pass_decrypt(args) {
+                Ok(pass_opts) => Ok(PasswordCommand::Decrypt(pass_opts)),
+                Err(e) => Err(e),
+            }
+        }
         _ => Err("Invalid argument".to_string()),
     }
 }
